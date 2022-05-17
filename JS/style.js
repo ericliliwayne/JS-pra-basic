539653369c434fd38ebe74d7ab94b8dd950eeba8
tung wei-i <ericliliwayne@gmail.com>
2022-05-17 21:37:38 +0800

    js基礎之練習1

---
+++ JS/style.js
@@ -0,0 +1,6 @@
+        //console.log,F12看console
+        console.log("hello world");
+        console.log("hello world");
+        console.log("hello world");
+        console.log("hello world");
+        alert("外部JS OK");
